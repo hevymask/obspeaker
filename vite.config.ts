4e36f97cedbb2f5a7500dbcbc6a11e168a3d2e6d
Hevy Mask <hevymask@gmail.com>
2026-08-28 22:50:42 +0900

    Type and library configuration correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import path from "path"
+import path from 'node:path'
 
 // Plugin
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST
 
 export default defineConfig(() => ({
@@ -31,12 +30,12 @@ export default defineConfig(() => ({
       ignored: ['**/src-tauri/**']
     },
 
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-
     clearScreen: false
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 }))
